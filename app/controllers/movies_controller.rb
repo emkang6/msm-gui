3a8d5b7ff6b0e1201f96f82a4fbbe332c3a99b7d
emkang6 <hkang6@uchicago.edu>
2024-11-10 22:36:41 +0000

    initial commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,23 @@
 class MoviesController < ApplicationController
+  
+  def create
+
+    # Parameters: {"query_title"=>"1", "query_year"=>"2", "query_duration"=>"3", "query_description"=>"4", "query_image"=>"5", "query_director_id"=>"6"}
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+  end
+
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
